1758bf8b2edb46422b7fed9d223fd2f0c69d1fde
Douglas-Dorug <douglasx30@gmail.com>
2021-09-15 18:44:40 -0300

    Modificaicados os prints

--- src/teste/HelloWorld.java
+++ src/teste/HelloWorld.java
@@ -2,6 +2,7 @@ package teste;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello darkness my old friend");
+        System.out.println("Hello darkness my meu vein amigo");
+        System.out.println("Shazan!!!!!!!");
     }
 }
